08763488d00056397169e3f064062519ebb514af
suhaim.k07 <suhaimkhalifabusiness07@gmail.com>
2026-08-12 00:58:16 +0530

    avg using class

--- .py
+++ .py
@@ -1,17 +1,19 @@
-# class Student:
-#     name = "suii"
+class Student:
 
-# s1 = Student()
-# print(s1.name)
 
-class Friends:
-    college_name = "anjuman"
     def __init__(self,name,marks):
         self.name = name
         self.marks = marks
+    @staticmethod
+    def static():
+        print("hello")
+        
+    def avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("hi", self.name,"your avg score is:", sum/3)
 
-s1 = Friends("siuu",99)
-s2 = Friends("suuii",99.6)
-print(s1.name,s1.marks)
-print(s2.name,s2.marks)
-print(s2.college_name)
+s1 = Student("affan",[44,33,22])
+s1.avg()
+s1.static()
