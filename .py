526c1ffefc55185796632e8a704bba60e199bd49
suhaim.k07 <suhaimkhalifabusiness07@gmail.com>
2026-08-11 10:41:13 +0530

    object

--- .py
+++ .py
@@ -1,5 +1,16 @@
-class Student:
-    name = "suii"
+# class Student:
+#     name = "suii"
 
-s1 = Student()
-print(s1.name)
+# s1 = Student()
+# print(s1.name)
+
+class Car:
+    colour = "red"
+    def __init__(self,name, rate):
+        self.name = name
+        self.rate = rate
+
+car1 = Car("bmv",9)
+car2 = Car("audi", 8.5)
+print(car2.rate)
+        
