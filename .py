654067b1e9dc51ef792cf88fb56a473ec53ae64e
suhaim.k07 <suhaimkhalifabusiness07@gmail.com>
2026-08-17 01:52:08 +0530

    credit and debit

--- .py
+++ .py
@@ -1,9 +1,19 @@
-bal1 = input ("enter the balance: ")
 class Account:
     def __init__(self,bal,acc_no):
         self.balance = bal
         self.account_no = acc_no
 
-acc1 = Account(bal1, 12345)
-print(acc1.balance)
-print(acc1.account_no)
+    def debit(self,amount):
+        self.balance -= amount
+        print("Rs. ",amount, "was debited")
+
+    def credit(self,amount):
+        self.balance += amount
+        print("Rs. ",amount,"was credited")
+    def final(self):
+        return self.balance
+
+acc1 = Account(1000,1234)
+acc1.debit(500)
+acc1.credit(5000)
+print(acc1.final())
