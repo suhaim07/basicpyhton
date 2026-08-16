ed90657990416c2b8139dc4da06bb8d9b1fa553a
suhaim.k07 <suhaimkhalifabusiness07@gmail.com>
2026-08-17 02:18:58 +0530

    private and public method

--- .py
+++ .py
@@ -1,19 +1,7 @@
 class Account:
-    def __init__(self,bal,acc_no):
-        self.balance = bal
-        self.account_no = acc_no
+    def __init__(self,acc_no,acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass
 
-    def debit(self,amount):
-        self.balance -= amount
-        print("Rs. ",amount, "was debited")
-
-    def credit(self,amount):
-        self.balance += amount
-        print("Rs. ",amount,"was credited")
-    def final(self):
-        return self.balance
-
-acc1 = Account(1000,1234)
-acc1.debit(500)
-acc1.credit(5000)
-print(acc1.final())
+acc1 = Account("1233","siuu")
+print(acc1.__acc_pass)
