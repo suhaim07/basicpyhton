1ac402f1f6e12149f7ac05e2c83665178d63d370
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-20 18:52:01 +0530

    employee role department and salary using oops

--- .py
+++ .py
@@ -1,13 +1,15 @@
-class Circle:
-    def __init__(self,radius):
-        self.radius = radius
+class Employee:
+    def __init__(self,role,dept,salary):
+        self.role = role
+        self.dept = dept
+        self.salary = salary
+    def ShowDetails(self):
+        print(f"work is {self.role} and department is {self.dept} and salary will be {self.salary}")
 
-    def area(self):
-        return 3.14 * self.radius ** 2
-
-    def perimeter(self):
-        return 2 * 3.14 * self.radius
-
-c1 = Circle(21)
-print(c1.area())
-print(c1.perimeter())
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("accountant","finance", 60000)
+e1 = Engineer("Affan",20)
+e1.ShowDetails()
