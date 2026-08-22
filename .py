80b3c957b67c7c8b9c6e0b4355207ef0f94c5d90
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-22 12:27:32 +0530

    maultiply all the elements

--- .py
+++ .py
@@ -1,15 +1,6 @@
 a = [1,2,3,4]
-n = 0
-b = a[n]
-c= a[0]
-
+n = a[0]
 for i in a:
-    if i > b:
-        b = i
-print(i)
+    n *= i
 
-for j in a:
-    if j < c:
-        c = j
-
-print(c)
+print(n)
