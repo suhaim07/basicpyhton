d31ae83d1ccc79f772d3975520a3e75048f077f4
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-23 02:17:52 +0530

    sum of only even number

--- .py
+++ .py
@@ -1,11 +1,8 @@
+
 a = [1,2,2,3,4,4,4]
-count = {}
+b = 0
 
 for i in a:
-    if i in count:
-        count[i] += 1
-
-    else:
-        count[i] = 1
-
-print(count)
+    if i % 2 == 0:
+         b +=i
+print(b)
