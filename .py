71bbff85d6320724e94172e2039b0e37b31ed983
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-24 02:10:58 +0530

    check whether odd or even

--- .py
+++ .py
@@ -1,6 +1,6 @@
-a = [1,2,2,3,4,4,4]
-square = []
-for i in a:
-    i **= 2
-    square.append(i)
-print(square)
+a = int(input("Enter the number: "))
+if a % 2 == 1:
+    print(f"{a} is odd")
+
+else:
+    print(f"{a} is even")
