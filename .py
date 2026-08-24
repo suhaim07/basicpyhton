2ae59dcd80908a6527b45eb9133d43efcc97d172
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-25 01:34:43 +0530

    reverse the integer

--- .py
+++ .py
@@ -1,15 +1,7 @@
-n = int(input("factorial of: "))
-fact = 1
-
-if n == 1:
-    print("0")
-elif n < 0:
-    print("-ve does not exist")
-for i in range(1,n+1):
-    fact *= i
-print(fact)
-
-
-
-
-
+n = 1234
+rev = 0
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
+print(rev)
