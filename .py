7437cfcf6cbc43793cadcba3ba3f6498363077d0
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-24 18:42:46 +0530

    to print tables of any number

--- .py
+++ .py
@@ -1,6 +1,5 @@
-a = int(input("Enter the number: "))
-if a % 2 == 1:
-    print(f"{a} is odd")
-
-else:
-    print(f"{a} is even")
+n = int(input("enter the table to print: "))
+c = 0
+for i in range(1,11):
+    c = i * n
+    print(f"{n} x {i} = {c}")
