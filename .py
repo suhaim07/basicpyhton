3bb21fbf509056bfeeb834376d83186127c09499
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-24 23:19:43 +0530

    find factorial

--- .py
+++ .py
@@ -1,6 +1,15 @@
-count = 0
-for i in range(1,101):
-    if i % 3 == 0:
-        count += 1
+n = int(input("factorial of: "))
+fact = 1
 
-print(count)
+if n == 1:
+    print("0")
+elif n < 0:
+    print("-ve does not exist")
+for i in range(1,n+1):
+    fact *= i
+print(fact)
+
+
+
+
+
