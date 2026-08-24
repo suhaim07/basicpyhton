e3e85003d963d0548e73a1843caae9165083a332
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-24 18:56:10 +0530

    numbers divisible by 3 from 1 to 100

--- .py
+++ .py
@@ -1,5 +1,6 @@
-n = int(input("enter the table to print: "))
-c = 0
-for i in range(1,11):
-    c = i * n
-    print(f"{n} x {i} = {c}")
+count = 0
+for i in range(1,101):
+    if i % 3 == 0:
+        count += 1
+
+print(count)
