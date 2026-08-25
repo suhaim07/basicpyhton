f31286bf750eae9225f2f8ca6c4e4146ea9d819c
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-26 01:46:23 +0530

    larget digit fromin

--- .py
+++ .py
@@ -1,13 +1,10 @@
-n = 1221
-original = n
-reversed_number = 0
+n = 58329
+largest_digit = 0
 
 while n > 0:
     digit = n % 10
-    reversed_number = reversed_number * 10 + digit
+    if digit > largest_digit:
+        largest_digit = digit
     n //= 10
 
-if original == reversed_number:
-    print("Palindrome")
-else:
-    print("Not a palindrome")
+print("Largest digit:", largest_digit)
