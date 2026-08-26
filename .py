18507934e482833634fc2c16e006a12486f96064
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-26 23:56:52 +0530

    count how many number occurs

--- .py
+++ .py
@@ -1,9 +1,7 @@
-n = 58329
-small_digit = n
+number = input("Enter a number: ")
+chosen_digit = input("Enter the digit to count (0-9): ")
 
-while n > 0:
-    digit = n % 10
-    if digit < small_digit:
-        small_digit = digit
-    n //= 10
-print(small_digit)
+if len(chosen_digit) != 1 or not chosen_digit.isdigit():
+    print("Please enter exactly one digit.")
+else:
+    print(f"The digit {chosen_digit} appears {number.count(chosen_digit)} time(s).")
