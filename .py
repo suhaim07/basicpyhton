5308b5c554580d46f008af1c25a3109c6833de76
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-29 00:55:24 +0530

    largest among 3 variables

--- .py
+++ .py
@@ -1,4 +1,9 @@
-for i in range(1, 6):
-    for j in range(i):
-        print(i, end="")
-    print()
+a = 10
+b = 20
+c= 5
+if a > b and a > c:
+    print("a is largest")
+elif b > a and b > c:
+    print("b is largest")
+else:
+    print("c is largest")
