d29988e235e7e178f35d0c344d2c66beabdcd8ff
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-29 07:02:08 +0530

    to print random number to from 1 to 10

--- .py
+++ .py
@@ -1,17 +1,5 @@
-'''code to check prime number or not'''
-number = int(input("enter the number: "))
+import random
 
-if number < 2:
-    print("Not prime")
-else:
-    is_prime = True
+c = random.randint(1,10)
 
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-
-    if is_prime:
-        print("Prime")
-    else:
-        print("Not prime")
+print(c)
