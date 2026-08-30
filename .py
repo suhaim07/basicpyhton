f0f60203e2b75e04413a26ffb34edb5c8083ae56
suhaim.k07 <suhaimkhalifa07@gmail.com>
2026-08-30 22:40:45 +0530

    to print prime in an interval

--- .py
+++ .py
@@ -1,5 +1,22 @@
-import random
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
 
-c = random.randint(1,10)
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-print(c)
+start = int(input("Enter the starting number of the interval: "))
+end = int(input("Enter the ending number of the interval: "))
+
+print(f"Prime numbers between {start} and {end} are:")
+for num in range(start, end + 1):
+    if is_prime(num):
+        print(num)
